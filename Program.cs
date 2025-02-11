12981ff09bbbca3f6e0b78fc0aafd3bf7b334f61
Ashu <ashu976sincere@gmail.com>
2025-02-11 13:10:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppDemoForGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
